a4e8827343e05d1bac2dfed4c9bfad58665fca77
yamdang <youm4000@naver.com>
2024-12-27 21:35:01 +0900

    feat: 에피그램 컴포넌트 height값 props로 전달 받도록 적용

--- epigram/src/components/share/Epigram.tsx
+++ epigram/src/components/share/Epigram.tsx
@@ -1,12 +1,21 @@
-export default function Epigram() {
+import clsx from 'clsx';
+
+interface EpigramProps {
+  height?: number;
+}
+
+export default function Epigram({ height }: EpigramProps) {
   return (
     <div className="mx-auto w-full max-w-[640px]">
-      <div className="mb-14">
+      <div style={{ marginBottom: height ? '0px' : '56px' }}>
         <div
-          className="mb-2 w-full rounded-2xl bg-cover bg-center bg-repeat-x p-6 shadow-[0_3px_12px_rgba(0,0,0,0.1)]"
-          style={{ backgroundImage: 'url(/images/back-line.png)' }}
+          className="mb-2 flex w-full flex-col justify-between gap-5 rounded-2xl bg-cover bg-center bg-repeat-x p-6 shadow-[0_3px_12px_rgba(0,0,0,0.1)]"
+          style={{
+            backgroundImage: 'url(/images/back-line.png)',
+            height: height && `${height}px`,
+          }}
         >
-          <p className="mb-5 text-left font-point text-xl font-medium text-black-600">
+          <p className="text-left font-point text-xl font-medium text-black-600">
             오랫동안 꿈을 그리는 사람은 마침내 그 꿈을 닮아 간다.
           </p>
           <span className="block text-right text-xl text-blue-400">
