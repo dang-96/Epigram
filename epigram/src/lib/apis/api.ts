047bce3ea003e6c6b009dfad4161d53659303996
yamdang <youm4000@naver.com>
2025-01-21 20:35:25 +0900

    feat: 회원가입 api 구현

---
+++ epigram/src/lib/apis/api.ts
@@ -0,0 +1,26 @@
+import axiosInstance from './instance';
+import { END_POINT } from './path';
+import { SignUpType } from '../types/type';
+
+// 회원가입 api
+export const fetchSignUp = async ({
+  email,
+  nickname,
+  password,
+  passwordCheck,
+}: SignUpType) => {
+  try {
+    const response = await axiosInstance.post(END_POINT.auth.signUp, {
+      email: email,
+      nickname: nickname,
+      password: password,
+      passwordConfirmation: passwordCheck,
+    });
+
+    console.log(response);
+    return response;
+  } catch (error: any) {
+    // console.log('회원가입 에러:', error.response.data.message);
+    throw new Error(error.response.data.message || '회원가입에 실패했습니다.');
+  }
+};
