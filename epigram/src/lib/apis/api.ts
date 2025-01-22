98c0a9804e18101375d6e522ed41b48e31f99049
yamdang <youm4000@naver.com>
2025-01-22 17:55:57 +0900

    feat: 로그인 api 구현

--- epigram/src/lib/apis/api.ts
+++ epigram/src/lib/apis/api.ts
@@ -1,6 +1,6 @@
 import axiosInstance from './instance';
 import { END_POINT } from './path';
-import { SignUpType } from '../types/type';
+import { LoginType, SignUpType } from '../types/type';
 
 // 회원가입 api
 export const fetchSignUp = async ({
@@ -17,10 +17,26 @@ export const fetchSignUp = async ({
       passwordConfirmation: passwordCheck,
     });
 
-    console.log(response);
+    // console.log(response);
     return response;
   } catch (error: any) {
     // console.log('회원가입 에러:', error.response.data.message);
     throw new Error(error.response.data.message || '회원가입에 실패했습니다.');
+  }
+};
+
+// 로그인 api
+export const fetchLogin = async ({ email, password }: LoginType) => {
+  try {
+    const response = await axiosInstance.post(END_POINT.auth.signIn, {
+      email: email,
+      password: password,
+    });
+
+    console.log(response.data);
+    return response.data;
+  } catch (error: any) {
+    console.log('로그인 api 에러', error.response.data.message);
+    throw new Error(error.response.data.message || '로그인에 실패했습니다.');
   }
 };
