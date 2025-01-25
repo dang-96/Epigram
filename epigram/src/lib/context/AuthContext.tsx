4fe8f0d1a418667690d8536628b072f1c4407604
yamdang <youm4000@naver.com>
2025-01-25 20:18:14 +0900

    feat: 로그인 상태를 전역으로 관리

---
+++ epigram/src/lib/context/AuthContext.tsx
@@ -0,0 +1,59 @@
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import { UserInfoType } from '../types/type';
+
+type AuthContextType = {
+  loginState: boolean;
+  user: UserInfoType | null;
+  login: (data: any) => void;
+  logout: () => void;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [loginState, setLoginState] = useState<boolean>(false);
+  const [user, setUser] = useState<UserInfoType | null>(null);
+
+  useEffect(() => {
+    const storageUserInfo = localStorage.getItem('userInfo');
+    const storageAccessToken = localStorage.getItem('accessToken');
+
+    if (storageUserInfo && storageAccessToken) {
+      setLoginState(true);
+      setUser(JSON.parse(storageUserInfo));
+    }
+  }, []);
+
+  const login = (data: any) => {
+    setLoginState(true);
+    setUser(data.user);
+
+    localStorage.setItem('userInfo', JSON.stringify(data.user));
+    localStorage.setItem('accessToken', data.accessToken);
+  };
+
+  const logout = () => {
+    setLoginState(false);
+    setUser(null);
+
+    localStorage.removeItem('userInfo');
+    localStorage.removeItem('accessToken');
+  };
+
+  return (
+    <AuthContext.Provider value={{ loginState, user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = (): any => {
+  const context = useContext(AuthContext);
+  return context;
+};
