ae1282b04684e7d024ee0020c35367e7315819fb
yamdang <youm4000@naver.com>
2025-01-25 21:21:38 +0900

    chore: 리액트 쿼리 설정

--- epigram/src/pages/_app.tsx
+++ epigram/src/pages/_app.tsx
@@ -2,14 +2,19 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import NavBar from '@/components/share/NavBar';
 import { AuthProvider } from '@/lib/context/AuthContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <AuthProvider>
-        <NavBar />
-        <Component {...pageProps} />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <NavBar />
+          <Component {...pageProps} />
+        </AuthProvider>
+      </QueryClientProvider>
     </>
   );
 }
