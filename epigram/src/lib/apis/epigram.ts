019ec4ea94545ec2b15b26ba1c541fb7d68d9712
yamdang <youm4000@naver.com>
2025-01-31 21:17:40 +0900

    feat: 최신 에피그램 api 구현

--- epigram/src/lib/apis/epigram.ts
+++ epigram/src/lib/apis/epigram.ts
@@ -1,3 +1,4 @@
+import { EpigramParamsType } from '../types/type';
 import axiosInstance from './instance';
 import { END_POINT } from './path';
 
@@ -8,5 +9,24 @@ export const fetchTodayEpigram = async () => {
     return response;
   } catch (error) {
     console.log('오늘의 에피그램 api 오류', error);
+  }
+};
+
+// 최신 에피그램(get)
+export const fetchNewEpigram = async ({
+  limit,
+  cursor = 0,
+}: EpigramParamsType) => {
+  try {
+    const response = await axiosInstance.get(END_POINT.epigram.base, {
+      params: {
+        limit: limit,
+        cursor: cursor,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log('최신 에피그램 api 오류', error);
+    throw new Error('최신 에피그램 데이터를 가져오는데 실패했습니다.');
   }
 };
