4c2a7befd0ab0428be4a45fb46e17c0decaf5997
yamdang <youm4000@naver.com>
2025-02-14 16:59:32 +0900

    feat: 에피그램 삭제 api 연동

--- epigram/src/components/detail/EpigramDetail.tsx
+++ epigram/src/components/detail/EpigramDetail.tsx
@@ -3,6 +3,9 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
+import { useUserInfo } from '@/lib/hooks/useUserInfo';
+import { deleteEpigram } from '@/lib/apis/epigram';
+import { useRouter } from 'next/router';
 
 interface EpigramDetailProps {
   data: EpigramDetailType | undefined;
@@ -11,9 +14,23 @@ interface EpigramDetailProps {
 
 export default function EpigramDetail({ data, isMore }: EpigramDetailProps) {
   const [isDropDown, setIsDropDown] = useState<boolean>(false);
+  const { userData } = useUserInfo();
+  const router = useRouter();
 
   const handleToggle = () => {
     setIsDropDown((prev) => !prev);
+  };
+
+  const handleDeleteEpigram = async (id: number) => {
+    try {
+      const res = await deleteEpigram(id);
+
+      router.push('/feed');
+      return res;
+    } catch (error) {
+      console.log('에피그램 삭제 api 호출 오류', error);
+      throw new Error('에피그램 삭제 api 호출에 실패했습니다.');
+    }
   };
   return (
     <div
@@ -23,7 +40,7 @@ export default function EpigramDetail({ data, isMore }: EpigramDetailProps) {
       <div className="mx-auto w-full max-w-[640px]">
         <div className="relative mb-8 flex items-center justify-between">
           <ul className="flex items-center gap-4 text-xl font-normal text-blue-400">
-            {data?.tags.map((tag: Tag) => {
+            {data?.tags?.map((tag: Tag) => {
               return <li key={tag.id}>#{tag.name}</li>;
             })}
           </ul>
@@ -38,24 +55,37 @@ export default function EpigramDetail({ data, isMore }: EpigramDetailProps) {
             </button>
           )}
 
-          <AnimatePresence>
-            {isDropDown && (
-              <motion.ul
-                initial={{ opacity: 0, y: -10 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: -10 }}
-                transition={{ duration: 0.3 }}
-                className="absolute right-0 top-[52px] overflow-hidden rounded-2xl border-[1px] border-blue-300 bg-background text-xl font-normal"
-              >
-                <li className="px-8 py-3">
-                  <button type="button">수정하기</button>
-                </li>
-                <li className="px-8 py-3">
-                  <button type="button">삭제하기</button>
-                </li>
-              </motion.ul>
-            )}
-          </AnimatePresence>
+          {data?.writerId === userData?.id ? (
+            <AnimatePresence>
+              {isDropDown && (
+                <motion.ul
+                  initial={{ opacity: 0, y: -10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -10 }}
+                  transition={{ duration: 0.3 }}
+                  className="absolute right-0 top-[52px] overflow-hidden rounded-2xl border-[1px] border-blue-300 bg-background text-xl font-normal"
+                >
+                  <li className="px-8 py-3">
+                    <button type="button">수정하기</button>
+                  </li>
+                  <li className="px-8 py-3">
+                    <button
+                      type="button"
+                      onClick={() => {
+                        if (data?.id !== undefined) {
+                          handleDeleteEpigram(data.id);
+                        }
+                      }}
+                    >
+                      삭제하기
+                    </button>
+                  </li>
+                </motion.ul>
+              )}
+            </AnimatePresence>
+          ) : (
+            ''
+          )}
         </div>
         <h2 className="mb-8 text-[32px] font-medium text-black-700">
           {data?.content}
@@ -77,7 +107,7 @@ export default function EpigramDetail({ data, isMore }: EpigramDetailProps) {
             {data?.likeCount}
           </button>
           <Link
-            href={data ? data?.referenceUrl : '/'}
+            href={data?.referenceUrl || '/'}
             className="flex h-12 items-center justify-center gap-[6px] rounded-[100px] bg-line-100 px-5 text-gray-300"
           >
             왕도로 가는길
