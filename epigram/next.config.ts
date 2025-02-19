eb0d425dd588b3914c9d5ff54ad2fb16a2a8cbde
yamdang <youm4000@naver.com>
2025-02-19 22:53:35 +0900

    feat: 외부 이미지 경로 추가

--- epigram/next.config.ts
+++ epigram/next.config.ts
@@ -1,8 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+        pathname: '/**', // 모든 경로를 허용
+      },
+    ],
+  },
 };
 
 export default nextConfig;
