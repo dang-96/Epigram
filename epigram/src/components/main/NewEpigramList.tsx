f81b455958190e880fbcbb7b7cff95153312ec40
yamdang <youm4000@naver.com>
2025-02-26 19:39:38 +0900

    feat: 에피그램 컴포넌트 재사용할 수 있도록 수정

--- epigram/src/components/main/NewEpigramList.tsx
+++ epigram/src/components/main/NewEpigramList.tsx
@@ -1,12 +1,31 @@
 import Link from 'next/link';
-import NewEpigram from './NewEpigram';
 import clsx from 'clsx';
+import { useQuery } from '@tanstack/react-query';
+import { fetchNewEpigram } from '@/lib/apis/epigram';
+import { EpigramType } from '@/lib/types/type';
+import Epigram from '../share/Epigram';
 
 interface NewEpigramProps {
   marginBottom?: string;
 }
 
 export default function NewEpigramList({ marginBottom }: NewEpigramProps) {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['newEpigram'],
+    queryFn: async () => {
+      const res = await fetchNewEpigram({ limit: 3, cursor: 0 });
+      console.log(res);
+      return res;
+    },
+  });
+
+  if (isLoading) {
+    return <div>로딩중...</div>;
+  }
+
+  if (isError) {
+    return <div>에러...</div>;
+  }
   return (
     <div
       className={clsx(
@@ -17,7 +36,9 @@ export default function NewEpigramList({ marginBottom }: NewEpigramProps) {
       <h2 className="mb-10 text-2xl font-semibold text-black-600">
         최신 에피그램
       </h2>
-      <NewEpigram />
+      {data?.list.map((epigram: EpigramType) => {
+        return <Epigram key={epigram.id} data={epigram} />;
+      })}
       <div className="flex justify-center">
         <Link
           href="/feed"
--- epigram/src/components/share/Epigram.tsx
+++ epigram/src/components/share/Epigram.tsx
@@ -1,5 +1,6 @@
 import { EpigramType } from '@/lib/types/type';
 import clsx from 'clsx';
+import { useRouter } from 'next/router';
 
 interface EpigramProps {
   data?: EpigramType;
@@ -7,18 +8,24 @@ interface EpigramProps {
 }
 
 export default function Epigram({ data, height }: EpigramProps) {
+  const router = useRouter();
+
+  const navigateToPage = () => {
+    router.push(`/feed/${data?.id}`);
+  };
   return (
     <div className="mx-auto w-full max-w-[640px]">
       <div style={{ marginBottom: height ? '0px' : '56px' }}>
         <div
           className={clsx(
-            'mb-2 flex w-full flex-col gap-5 rounded-2xl bg-cover bg-center bg-repeat-x p-6 shadow-[0_3px_12px_rgba(0,0,0,0.1)]',
+            'mb-2 flex w-full cursor-pointer flex-col gap-5 rounded-2xl bg-cover bg-center bg-repeat-x p-6 shadow-[0_3px_12px_rgba(0,0,0,0.1)]',
             data ? 'justify-between' : 'justify-center'
           )}
           style={{
             backgroundImage: 'url(/images/back-line.png)',
             height: height && `${height}px`,
           }}
+          onClick={navigateToPage}
         >
           {data ? (
             <>
@@ -39,7 +46,7 @@ export default function Epigram({ data, height }: EpigramProps) {
         </div>
         {data && (
           <ul className="flex items-center justify-end gap-4">
-            {data?.tags.map((tag) => {
+            {data?.tags?.map((tag) => {
               return (
                 <li key={tag.id} className="text-xl font-medium text-blue-400">
                   #{tag.name}
