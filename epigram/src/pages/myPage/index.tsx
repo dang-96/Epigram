bb5d5dd04cdae356c50cd2e7431b29f3ef99d730
yamdang <youm4000@naver.com>
2025-02-26 16:55:39 +0900

    feat: ts-pattern을 사용하여 마이페이지 탭 메뉴 구현

--- epigram/src/pages/myPage/index.tsx
+++ epigram/src/pages/myPage/index.tsx
@@ -1,8 +1,18 @@
 import NewEpigram from '@/components/main/NewEpigram';
+import NewEpigramList from '@/components/main/NewEpigramList';
 import TodayState from '@/components/main/TodayState';
 import Profile from '@/components/myPage/Profile';
+import { useState } from 'react';
+import { match } from 'ts-pattern';
 
 export default function MyPage() {
+  const EPIGRAM = 'epigram';
+  const COMMENT = 'comment';
+  const [myPageTab, setMyPageTab] = useState<string>(EPIGRAM);
+
+  const handleTab = (tab: string) => {
+    setMyPageTab(tab);
+  };
   return (
     <div className="relative bg-background py-32">
       <div className="w-ful mb-24 h-full rounded-3xl bg-white pb-20">
@@ -23,10 +33,17 @@ export default function MyPage() {
       </div>
       <div className="mx-auto w-full max-w-[640px]">
         <div className="mb-12">
-          <button type="button">내 에피그램(11)</button>
-          <button type="button">내 댓글(11)</button>
+          <button type="button" onClick={() => handleTab(EPIGRAM)}>
+            내 에피그램(11)
+          </button>
+          <button type="button" onClick={() => handleTab(COMMENT)}>
+            내 댓글(11)
+          </button>
         </div>
-        <NewEpigram marginBottom={'mb-0'} />
+        {match(myPageTab)
+          .with(EPIGRAM, () => <NewEpigram />)
+          .with(COMMENT, () => <div>댓글 리스트</div>)
+          .otherwise(() => '')}
       </div>
     </div>
   );
