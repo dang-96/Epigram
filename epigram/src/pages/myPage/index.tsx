3a70f06a2010f8427bcb59d7f604c95a7d7ea82e
yamdang <youm4000@naver.com>
2025-02-26 19:41:58 +0900

    feat: 마이페이지 나의 에피그램과 댓글 리스트 영역 구현

--- epigram/src/pages/myPage/index.tsx
+++ epigram/src/pages/myPage/index.tsx
@@ -1,18 +1,8 @@
-import NewEpigram from '@/components/main/NewEpigram';
-import NewEpigramList from '@/components/main/NewEpigramList';
 import TodayState from '@/components/main/TodayState';
+import ContentAllList from '@/components/myPage/ContentAllList';
 import Profile from '@/components/myPage/Profile';
-import { useState } from 'react';
-import { match } from 'ts-pattern';
 
 export default function MyPage() {
-  const EPIGRAM = 'epigram';
-  const COMMENT = 'comment';
-  const [myPageTab, setMyPageTab] = useState<string>(EPIGRAM);
-
-  const handleTab = (tab: string) => {
-    setMyPageTab(tab);
-  };
   return (
     <div className="relative bg-background py-32">
       <div className="w-ful mb-24 h-full rounded-3xl bg-white pb-20">
@@ -31,20 +21,7 @@ export default function MyPage() {
           </div>
         </div>
       </div>
-      <div className="mx-auto w-full max-w-[640px]">
-        <div className="mb-12">
-          <button type="button" onClick={() => handleTab(EPIGRAM)}>
-            내 에피그램(11)
-          </button>
-          <button type="button" onClick={() => handleTab(COMMENT)}>
-            내 댓글(11)
-          </button>
-        </div>
-        {match(myPageTab)
-          .with(EPIGRAM, () => <NewEpigram />)
-          .with(COMMENT, () => <div>댓글 리스트</div>)
-          .otherwise(() => '')}
-      </div>
+      <ContentAllList />
     </div>
   );
 }
