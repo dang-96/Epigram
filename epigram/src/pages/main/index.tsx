81d07786a5547d319490733deaa5cae21900a66c
yamdang <youm4000@naver.com>
2025-03-25 19:24:07 +0900

    feat: 메인 페이지 로그인 안내 컴포넌트 적용

--- epigram/src/pages/main/index.tsx
+++ epigram/src/pages/main/index.tsx
@@ -3,15 +3,22 @@ import TodayEpigram from '@/components/main/TodayEpigram';
 import TodayState from '@/components/main/TodayState';
 import FixedMenu from '@/components/share/FixedMenu';
 import NewEpigramList from '@/components/main/NewEpigramList';
+import LoginMessage from '@/components/share/LoginMessage';
+import { useAuth } from '@/lib/context/AuthContext';
 
 export default function MainPage() {
+  const { loginState } = useAuth();
+
+  if (!loginState) {
+    return <LoginMessage />;
+  }
+
   return (
     <div className="h-full bg-background py-[120px]">
       <TodayEpigram />
       <TodayState />
       <NewEpigramList />
       <NewCommentList />
-      <FixedMenu />
     </div>
   );
 }
