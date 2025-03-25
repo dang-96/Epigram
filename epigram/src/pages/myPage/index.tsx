5bf6aeaf1d8eaec8d338b6d062b7383c9deca335
yamdang <youm4000@naver.com>
2025-03-25 19:23:43 +0900

    feat: 마이페이지 로그인 안내 컴포넌트 적용

--- epigram/src/pages/myPage/index.tsx
+++ epigram/src/pages/myPage/index.tsx
@@ -3,8 +3,16 @@ import CalendarEmotion from '@/components/myPage/CalendarEmotion';
 import ChartEmotion from '@/components/myPage/ChartEmotion';
 import ContentAllList from '@/components/myPage/ContentAllList';
 import Profile from '@/components/myPage/Profile';
+import LoginMessage from '@/components/share/LoginMessage';
+import { useUserInfo } from '@/lib/hooks/useUserInfo';
 
 export default function MyPage() {
+  const { loginState } = useUserInfo();
+
+  if (!loginState) {
+    return <LoginMessage />;
+  }
+
   return (
     <div className="relative bg-background py-32">
       <div className="w-ful mb-24 h-full rounded-3xl bg-white pb-20">
