bf4186c11f25164bc2b4c6a202a4278910bf899b
yamdang <youm4000@naver.com>
2025-03-25 19:26:57 +0900

    feat: 우측 고정 메뉴 적용 페이지 수정

--- epigram/src/pages/_app.tsx
+++ epigram/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import NavBar from '@/components/share/NavBar';
 import { AuthProvider } from '@/lib/context/AuthContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import FixedMenu from '@/components/share/FixedMenu';
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <AuthProvider>
           <NavBar />
           <Component {...pageProps} />
+          <FixedMenu />
         </AuthProvider>
       </QueryClientProvider>
     </>
